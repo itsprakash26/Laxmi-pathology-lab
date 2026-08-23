9535268dcdae4f7d7dfba66a1df18d622bf86c8a
om prakash choudhary <omprakeshchaudhary01@gmail.com>
2026-08-24 02:00:36 +0530

    update homecollection card

--- laxmi-pathology-lab/src/App.jsx
+++ laxmi-pathology-lab/src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import PopularTests from "./components/PopularTests";
+import HowItWorks from "./components/HowItWorks";
+import HomeCollection from "./components/HomeCollection";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <PopularTests />
+      <HowItWorks />
+      <HomeCollection />
     </>
   );
 }
