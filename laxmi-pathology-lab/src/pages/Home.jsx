d9425ae6b3c83a77b2cbf6f683a9473743012055
om prakash choudhary <omprakeshchaudhary01@gmail.com>
2026-08-24 04:23:01 +0530

    update Home Page

---
+++ laxmi-pathology-lab/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero";
+import PopularTests from "../components/PopularTests";
+import HowItWorks from "../components/HowItWorks";
+import HomeCollection from "../components/HomeCollection";
+import AboutUs from "../components/AboutUs";
+import Contact from "../components/Contact";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <PopularTests />
+      <HowItWorks />
+      <HomeCollection />
+      <AboutUs />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
